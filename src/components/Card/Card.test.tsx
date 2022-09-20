9b781fa344e2926a95fba4f298aaa9b79193d45b
GenaVinokurov <genabrest97@gmail.com>
2022-09-20 13:17:50 +0300

    refactor: redux to main page according stylelint

--- src/components/Card/Card.test.tsx
+++ src/components/Card/Card.test.tsx
@@ -1,9 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import Card from './Card';
-import data from '../../mockedData/data-movies.json';
 import { Provider } from 'react-redux';
 import { setupStore } from '../../store/store';
+import Card from './Card';
+import data from '../../mockedData/data-movies.json';
 
 describe('card tests', () => {
   const store = setupStore();
