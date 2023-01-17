9304c344ade2370c4afa59f23c72696c3749716d
GenaVinokurov <genabrest97@gmail.com>
2023-01-17 11:21:20 +0100

    add: new test in Search component

--- src/tests/components/Search.test.tsx
+++ src/tests/components/Search.test.tsx
@@ -1,16 +1,38 @@
-import { screen } from '@testing-library/react';
+import { act, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import React from 'react';
 import renderWithProviders from '../../mockedData/test-utils';
 import Search from '../../components/Search/Search';
+import { useNavigate } from 'react-router-dom';
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: jest.fn(),
+}));
 
 describe('Search', () => {
-  it('change value', () => {
+  it('should change value', () => {
     renderWithProviders(<Search />);
     const search = screen.getByPlaceholderText(/search/i) as HTMLInputElement;
+
     expect(search).toBeInTheDocument();
     expect(search).not.toHaveFocus();
+
     userEvent.type(search, 'Movie');
+
     expect(search.value).toBe('Movie');
+  });
+
+  it('should make navigate after press Enter button', () => {
+    const navigateMock = jest.fn();
+    (useNavigate as jest.Mock).mockReturnValue(navigateMock);
+
+    renderWithProviders(<Search />);
+    const search = screen.getByPlaceholderText(/search/i) as HTMLInputElement;
+
+    act(() => search.focus());
+    userEvent.keyboard('{Enter}');
+
+    expect(navigateMock).toBeCalled();
   });
 });
