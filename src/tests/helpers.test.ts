da31114033b531c81a16353eb766547ff3a175ae
GenaVinokurov <genabrest97@gmail.com>
2023-01-31 14:05:36 +0100

    add: tests to convertGenresString function

--- src/tests/helpers.test.ts
+++ src/tests/helpers.test.ts
@@ -1,8 +1,24 @@
-import { getTimeFromMins } from '../helpers';
+import { getTimeFromMins, convertGenresToString } from '../helpers';
 
 describe('helper functions', () => {
   it('test getTimeFromMins function', () => {
     expect(getTimeFromMins(100)).toBe('1:40');
     expect(getTimeFromMins(60)).toBe('1:0');
+  });
+
+  it('test convertGenresToString', () => {
+    const genresArray = [
+      {
+        id: 0,
+        name: 'action',
+      },
+      {
+        id: 1,
+        name: 'historical',
+      },
+    ];
+    expect(convertGenresToString([0], genresArray)).toBe('action ');
+    expect(convertGenresToString([0, 1], genresArray)).toBe('action historical ');
+    expect(convertGenresToString([], genresArray)).toBe('');
   });
 });
